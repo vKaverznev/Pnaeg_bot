7a1fc38ceff22b28ed8b64269998fedff583dc5a
vKaverznev <136694861+vKaverznev@users.noreply.github.com>
2024-09-27 11:18:44 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import telegram
 import os
+from telegram.error import RetryAfter, TelegramError
 
 # Инициализация Flask-приложения
 app = Flask(__name__)
@@ -19,8 +20,20 @@ bot = telegram.Bot(token=TOKEN)
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 if HEROKU_APP_NAME:
     WEBHOOK_URL = f'https://{HEROKU_APP_NAME}.herokuapp.com/{TOKEN}'
-    bot.set_webhook(url=WEBHOOK_URL)
-    print(f"Вебхук установлен на {WEBHOOK_URL}")
+    try:
+        # Проверяем, установлен ли вебхук
+        webhook_info = bot.get_webhook_info()
+        if webhook_info.url != WEBHOOK_URL:
+            bot.set_webhook(url=WEBHOOK_URL)
+            print(f"Вебхук установлен на {WEBHOOK_URL}")
+        else:
+            print(f"Вебхук уже установлен на {WEBHOOK_URL}")
+    except RetryAfter as e:
+        print(f"Превышено ограничение на установку вебхука. Повторите через {e.retry_after} секунд.")
+    except TelegramError as e:
+        print(f"Ошибка Telegram: {e}")
+    except Exception as e:
+        print(f"Непредвиденная ошибка: {e}")
 else:
     print("Ошибка: Переменная HEROKU_APP_NAME не установлена.")
 
